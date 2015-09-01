193ce435f3b01734dd0d8daf9a06bc26977a21da
Komal Rathi <komalsrathi@users.noreply.github.com>
2015-09-01 15:06:05 -0400

    rename RNASeqV2 normalized counts file from TCGA

--- rename_normalizedcountfile.R
+++ rename_normalizedcountfile.R
@@ -1,7 +1,10 @@
 ################################ download rsem.genes.normalized_results ###################################
 
+# move all normalized counts to genecounts folder
 system('mv *.rsem.genes.normalized_results ./genecounts/')
+# make a file to rename the existing namings 
 system('cut -f5,7 file_manifest.txt | grep rsem.genes.normalized_results > file_rename.txt')
+# rename count files according to sample names
 ren <- read.table('file_rename.txt')
 file.rename(from=list.files(pattern=".rsem.genes.normalized_results"),to=list(as.character(ren$V1))[[1]])
 
