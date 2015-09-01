831591272ef1737cb9de2add32de8b87697d4da8
Komal Rathi <komalsrathi@users.noreply.github.com>
2015-09-01 12:00:59 -0400

    Prepare TCGA Annotation file

--- prepTCGA_Annotation.R
+++ prepTCGA_Annotation.R
@@ -1,15 +1,5 @@
-
-################################ download rsem.genes.normalized_results ###################################
-
-# mv *.rsem.genes.normalized_results ./genecounts/
-# cut -f5,7 file_manifest.txt | grep rsem.genes.normalized_results > file_rename.txt
-# ren <- read.table('file_rename.txt')
-# file.rename(from=list.files(pattern=".rsem.genes.normalized_results"),to=list(as.character(ren$V1))[[1]])
-
-################################ download rsem.genes.normalized_results ###################################
-
 ################################ prep up the TCGA annotation ##############################################                                                 
-# grep 'gene' TCGA.Sept2010.09202010.gaf | cut -f16,17 > TCGA.Sept2010.09202010.gene.gaf [Gene & GeneLocus]
+system('grep \'gene\' TCGA.Sept2010.09202010.gaf | cut -f16,17 > TCGA.Sept2010.09202010.gene.gaf') # [Gene & GeneLocus]
 tcga = read.delim('TCGA.Sept2010.09202010.gene.gaf',header=F)
 s <- strsplit(as.character(tcga$V2), ';')
 final = data.frame(V1 = rep(tcga$V1, sapply(s, length)), V2 = unlist(s))
@@ -22,5 +12,5 @@ final = final[,c(1,3,5,6,7)]
 final = ddply(final, .(V1,chr,strand), summarise, start= min(start), end= max(end))
 final$score = 0
 final = final[,c(2,4,5,1,6,3)]
-# write.table(final,'TCGA.Sept2010.09202010.gene.bed',row.names=F,col.names=T,sep="\t",quote=F)
+write.table(final,'TCGA.Sept2010.09202010.gene.bed',row.names=F,col.names=T,sep="\t",quote=F)
 ################################ prep up the TCGA annotation ##############################################   
